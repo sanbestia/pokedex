504513626acbb4e8007e99f1e746046494d18fa9
sanbestia <sebayanic@gmail.com>
2026-05-05 16:24:29 -0300

    add REPL logic

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,31 @@
+import { createInterface } from 'readline';
+
+
 export function cleanInput(input: string): string[] {
+    // Transform a string of words into a list of lowercase words without whitespace
     return input.toLowerCase().trim().split(/\s+/);
+}
+
+export function startREPL() {
+    // Create interface to interact with the console stream
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Pokedex > '
+    });
+
+    rl.prompt();
+
+    rl.on('line', (input) => {
+        const wordArr: string[] = cleanInput(input);
+        if (wordArr[0] === "") {
+            rl.prompt();
+        } else {
+            console.log(`Your command was: ${wordArr[0]}`);
+            rl.prompt();
+        };
+    });
+
+
+
 }
